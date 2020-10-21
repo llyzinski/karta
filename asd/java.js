509d45f501779f5f2e6af8d6fb172f95f2a98c3f
llyzinski <42960684+llyzinski@users.noreply.github.com>
2020-10-21 10:19:34 +0200

    Add files via upload

--- asd/java.js
+++ asd/java.js
@@ -1,13 +1,20 @@
 const follow={
     numer: document.getElementById("numer"),
     przy: document.getElementById("przy"),
+    przy1: document.getElementById("przy1"),
 
     btn1:function(){
         this.numer.innerHTML=parseInt(this.numer.innerHTML)+1;
     },
 
+    
+    btn2:function(){
+        this.numer.innerHTML=parseInt(this.numer.innerHTML)-1;
+    },
+
     init:function(){
         this.przy.addEventListener('click',this.btn1.bind(follow));
+        this.przy1.addEventListener('click',this.btn2.bind(follow));
     },
 }
 follow.init();
